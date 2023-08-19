39698c980f71b78e80391e4a383b08d05881dd73
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-08-19 19:18:14 +0530

    Create BFS of graph.cpp

---
+++ BFS of graph.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> ans;
+        vector<bool>visited(V,false);
+        queue<int> q;
+        q.push(0);
+        visited[0]=1;
+        while(!q.empty())
+        {
+            int value=q.front();
+            q.pop();
+            ans.push_back(value);
+            for(auto x: adj[value])
+            {
+                if(visited[x]==0)
+                {
+                    q.push(x);
+                    visited[x]=1;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
